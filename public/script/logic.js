de03fac77c0d48d1d033ad5ac38571dfeaa5a4e7
omar-shaath <omarshaath89@gmail.com>
2021-08-11 20:58:17 +0300

    added DOM and Logic data

---
+++ public/script/logic.js
@@ -0,0 +1,11 @@
+
+const fetching = (data)=>{
+return fetch('/search',
+ {
+ method: 'POST',
+ headers: {'Content-Type': 'application/json'},
+ body: JSON.stringify(data)
+})
+.then((res)=>res.json())
+.then((weatherData)=>fetchWeather(weatherData));
+}
