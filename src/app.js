9e166c9f30b811114b9c763af2720b4832b49e7a
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-08-11 13:13:33 +0300

    require add-fetch

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 
 const express = require("express");
 const path = require("path");
+const fetch = require('node-fetch');
+const getWeather = require("./controller/getWeather")
+require("dotenv").config();
+
 const app = express();
+
+app.set('port' , process.env.PORT || 4000);
 
 app.disable("x-powered-by");
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','public')));
 
-app.set('port' , process.env.PORT || 3000);
+
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,"..","public"))
 })
+app.post('/search' ,getWeather) ;
 
 
 module.exports=app;
