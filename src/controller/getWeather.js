418cae2476f4c004940d10ecfd7c270a299d0504
omar-shaath <omarshaath89@gmail.com>
2021-08-12 10:53:22 +0300

    hiding the api key Relates #16

--- src/controller/getWeather.js
+++ src/controller/getWeather.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const getWeather = (req,res) => {
     const data = req.body.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${data}&units=metric&apikey=cf9fb4df751879d3f30929a3dd9050e4`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${data}&units=metric&apikey=${process.env.API_KEY}`
     fetch(url)
     .then(res => res.json())
     .then(body => res.json(body));
