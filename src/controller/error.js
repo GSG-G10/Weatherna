1739c9f1ea6c3086e3d88e9e26ef4c030f922d02
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-08-12 10:09:22 +0300

    add the 4th arguments Relates #19

--- src/controller/error.js
+++ src/controller/error.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const serverError = (err,req,res) => {
+const serverError = (err,req,res,next) => {
     res.status(500).sendFile(path.join(__dirname , "..", "public","500.html"));
 }
 const clientError = (req,res,next) => {
