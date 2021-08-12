178386b4dbe77a8a20f7646e897edd809b4597dc
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-08-12 09:02:17 +0300

    add on the file file

--- public/script/dom.js
+++ public/script/dom.js
@@ -7,19 +7,22 @@ const weatherStatus = document.getElementById("weather-status");
 const windSpeed = document.getElementById("wind-speed");
 const humidity = document.getElementById("humidity");
 const weatherDev = document.getElementById("weatherDev");
+const flag = document.getElementById("flag");
+
 fetching({ value: "palestine" });
 const fetchWeather = (weatherData) => {
   if (weatherData.cod == 404) {
     eror.textContent = weatherData.message;
     weatherDev.style.display = "none";
   } else if (weatherData.cod == 200) {
-    weatherDev.style.display = "block";
-    countryName.innerText = "Country Name: " + weatherData.name;
+    weatherDev.style.display = "flex";
+    countryName.innerText =  weatherData.name;
     temp.innerText = "Temperature " + weatherData.main.temp + "C°";
     weatherStatus.innerText =
       "Weather Status: " + weatherData.weather[0].description;
     windSpeed.innerText = "Wind Speed: " + weatherData.wind.speed + "m/s";
     humidity.innerText = "Humidity: " + weatherData.main.humidity + "%";
+    flag.src = `https://www.countryflags.io/${weatherData.sys.country}/shiny/64.png`
   }
 };
 
@@ -27,10 +30,12 @@ search.addEventListener("click", (event) => {
   const value = city.value;
   if (value === "" || (value.startsWith(" ") && value.endsWith(" "))) {
     eror.textContent = "Insert valid country and search again";
+    weatherDev.style.display = "none";
   } else {
     eror.textContent = "";
     const data = { value: value.trim() };
     fetching(data);
+    
   }
   event.preventDefault();
 });
